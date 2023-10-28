61e4b655b68c5cb7cca039487a554bb7747ae86f
Ameya_Ade <ameyaaade@gmail.com>
2023-10-28 19:32:26 -0400

    trail animation

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function drawCircle(x, y, radius = 10) {
   if (!y) {
     y = mouse.y;
   }
-  ctx.fillStyle = "Blue";
+  ctx.fillStyle = "white";
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, Math.PI * 2);
   ctx.fill();
@@ -34,15 +34,18 @@ canvas.addEventListener("click", function (event) {
   mouse.x = x;
   mouse.y = y;
 
-    for (let i = 0; i < 10; i++) {
-      particles.push(new Particle());
-    }
+  for (let i = 0; i < 10; i++) {
+    particles.push(new Particle());
+  }
 });
 
 canvas.addEventListener("mousemove", function (event) {
   const { x, y } = event;
   mouse.x = x;
   mouse.y = y;
+  for (let i = 0; i < 10; i++) {
+    particles.push(new Particle());
+  }
 });
 
 canvas.addEventListener("mouseleave", function (event) {
@@ -53,10 +56,10 @@ canvas.addEventListener("mouseleave", function (event) {
 
 class Particle {
   constructor() {
-    this.x = mouse.x,
-    this.y = mouse.y,
-    // (this.x = Math.random() * canvas.width),
-    //   (this.y = Math.random() * canvas.height),
+    (this.x = mouse.x),
+      (this.y = mouse.y),
+      // (this.x = Math.random() * canvas.width),
+      //   (this.y = Math.random() * canvas.height),
       (this.size = Math.random() * 10 + 2),
       (this.moveX = Math.random() * 3 - 1.5);
     this.moveY = Math.random() * 3 - 1.5;
@@ -66,27 +69,25 @@ class Particle {
     this.x = this.x + this.moveX;
     this.y = this.y + this.moveY;
     if (this.size > 0.2) {
-        this.size = this.size - 0.1;
+      this.size = this.size - 0.1;
     }
   }
 
   draw() {
-    ctx.fillStyle = "Blue";
+    ctx.fillStyle = "white";
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill();
   }
 }
 
-
-
 function handleParticles() {
   for (let i = 0; i < particles.length; i++) {
     particles[i].update();
     particles[i].draw();
     if (particles[i].size <= 0.3) {
-        particles.splice(i,1); // remove the particle if size is less than 0.3
-        i--; // shrink the size of array i.e loop
+      particles.splice(i, 1); // remove the particle if size is less than 0.3
+      i--; // shrink the size of array i.e loop
     }
   }
 }
